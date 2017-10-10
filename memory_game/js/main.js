4a9d24d9366b3942a14e9e22df267c2cb7bc8155
Ross Davis <rossjdavis@gmail.com>
2017-10-10 13:29:43 -0400

    added scoreboard and fixed layout

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -24,6 +24,9 @@ var cards = [
 ];
 
 var cardsInPlay = [];
+var randomIndex = [];
+
+var tempCardsId = -1;
 
 var score = 0;
 var attempts = 0;
@@ -31,9 +34,13 @@ var attempts = 0;
 var checkForMatch = function () {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		score++;
-		alert("You found a match " + score + " out of " + attempts + " attempts!");		
+		//alert("You found a match " + score + " out of " + attempts + " attempts!");		
+		document.getElementById("alert").innerHTML = "You found a match!";
+		document.getElementById("score").innerHTML = parseInt(score) + "|" + parseInt(attempts-score);
 	} else {
-		alert("Sorry, try again!");		
+		//alert("Sorry, try again!");		
+		document.getElementById("alert").innerHTML = "Sorry, try again.";
+		document.getElementById("score").innerHTML = parseInt(score) + "|" + parseInt(attempts-score);
 	}
 };
 
@@ -44,7 +51,14 @@ var flipCard = function () {
 	console.log(cards[cardId].suit);
 	console.log(cards[cardId].cardImage);
 	this.setAttribute('src', cards[cardId].cardImage);
-	if (cardsInPlay.length === 2) {
+	
+	if (tempCardsId === -1) {
+		tempCardsId = cardId;
+	} else if (tempCardsId === cardId) {
+		//alert("You cannot select the same card, cheater!");
+		document.getElementById("alert").innerHTML = "You cannot select the same card.";
+		resetBoard();
+	} else if (cardsInPlay.length === 2) {
 		attempts++;
 		checkForMatch();
 	}
@@ -55,7 +69,6 @@ var resetBoard = function () {
 
 	//location.reload();
 	
-	
 	var gameBoard = document.getElementById('game-board');
 	while (gameBoard.hasChildNodes()) {
 		gameBoard.removeChild(gameBoard.firstChild);
@@ -64,22 +77,44 @@ var resetBoard = function () {
 	while (cardsInPlay.length > 0) {
 		cardsInPlay.pop();
 	}
+
+	while (randomIndex.length > 0) {
+		randomIndex.pop();
+	}
+
+	tempCardsId = -1;
 	
 	createBoard();
 	
 }
 
+var shuffleDeck = function () {
+	
+	var randomCardId;
+	while (randomIndex.length < cards.length) {
+		randomCardId = Math.floor(Math.random()*cards.length);
+		if (randomIndex.indexOf(randomCardId) > -1) {
+			continue;
+		} else {
+			randomIndex.push(randomCardId);
+		}
+	}
+}
+
 var createBoard = function () {
+	shuffleDeck();
+
 	for (var i = 0; i < cards.length; i++) {
 		var cardElement = document.createElement('img');
 		cardElement.setAttribute('src', "images/back.png");
-		cardElement.setAttribute('data-id', i);
+		cardElement.setAttribute('data-id', randomIndex[i]);
 		cardElement.addEventListener('click', flipCard);
 		document.getElementById('game-board').appendChild(cardElement);
 
-		var resetButton = document.getElementById('reset-board');
-		resetButton.addEventListener('click', resetBoard);
 	}
+
+	var resetButton = document.getElementById('reset-board');
+	resetButton.addEventListener('click', resetBoard);
 };
 
 createBoard();
